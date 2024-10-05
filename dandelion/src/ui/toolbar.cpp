aee09f091b56b6b9801c1fd63c2ad2d47c88b43b
Rouge Lin <rouge_3877@outlook.com>
2024-10-05 21:06:35 +0800

    Complete Lab2.1 (Eular2Quaternion-lab): Refactor Toolbar::layout_mode()
    Refactor Toolbar::layout_mode() to eliminate the dependence on AngleAxisf() from the Eigen library when converting from Euler angles to quaternions in this method.

--- dandelion/src/ui/toolbar.cpp
+++ dandelion/src/ui/toolbar.cpp
@@ -183,9 +183,42 @@ void Toolbar::layout_mode(Scene& scene)
                              ImGuiSliderFlags_AlwaysClamp);
             ImGui::PopItemWidth();
             ImGui::PopID();
-            selected_object->rotation = AngleAxisf(radians(x_angle), Vector3f::UnitX()) *
-                                        AngleAxisf(radians(y_angle), Vector3f::UnitY()) *
-                                        AngleAxisf(radians(z_angle), Vector3f::UnitZ());
+
+            // Following given Expression is used to transform euler angles to quaternion by using AngleAxisf(Eigen)
+            //
+            // selected_object->rotation = AngleAxisf(radians(x_angle), Vector3f::UnitX()) *
+            //                             AngleAxisf(radians(y_angle), Vector3f::UnitY()) *
+            //                             AngleAxisf(radians(z_angle), Vector3f::UnitZ());
+            //
+            // TODO: Implement the transformation from euler angles to quaternion without AngleAxisf
+            // 
+            // Solution:
+            //      A quaternion has two part, a scalar part(real-`w`) and a vector part(imaginary-`[i,j,k]`),
+            //      We can associate a quaternion with a rotation around an axis by the following expression[1]
+            //          q_x = \sin(\theta_x/2) * cos(\beta_x)
+            //          q_y = \sin(\theta_y/2) * cos(\beta_y)
+            //          q_z = \sin(\theta_z/2) * cos(\beta_z)
+            //      In our case, x => pitch, y => yaw, z => roll
+            //          q_x = [\cos(\pitch/2), \sin(\pitch/2), 0, 0]
+            //          q_y = [\cos(\yaw/2), 0, \sin(\yaw/2), 0]
+            //          q_z = [\cos(\roll/2), 0, 0, \sin(\roll/2)]
+            //      Thus, the quaternion can be represented as the following quaternion multiplication:
+            //          q = q_x * q_y * q_z
+            //
+            // Citation: 
+            //      - [1] https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
+            //      - [2] https://ntrs.nasa.gov/api/citations/19770019231/downloads/19770019231.pdf
+            //
+
+            auto x_angle_rad = radians(x_angle);
+            auto y_angle_rad = radians(y_angle);
+            auto z_angle_rad = radians(z_angle);
+
+            Quaternionf q_x(std::cos(x_angle_rad / 2), std::sin(x_angle_rad / 2), 0, 0);
+            Quaternionf q_y(std::cos(y_angle_rad / 2), 0, std::sin(y_angle_rad / 2), 0);
+            Quaternionf q_z(std::cos(z_angle_rad / 2), 0, 0, std::sin(z_angle_rad / 2));
+
+            selected_object->rotation = q_x * q_y * q_z;
         }
         ImGui::EndTabItem();
     }
